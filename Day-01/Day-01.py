efc85ec653433bfc0efba38206ea720367f13255
nehalsoam <nehalnehal8434@gmail.com>
2026-08-25 00:22:45 +0530

    Day 01: Python practice

---
+++ Day-01/Day-01.py
@@ -0,0 +1,40 @@
+# Q1->print and store name,age,city in variable
+your_name="Nehal"
+your_age=22
+your_city="Indore"
+print(your_name)
+print(your_age)
+print(your_city)
+
+'''
+Q2->take two numbers a and b from user and print their sum ,difference
+multiplication and division
+'''
+a=int(input("enter a number "))
+b=int(input("enter a number "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#Q3->find area and perimeter of rectangle
+length=int(input("enter a length "))
+width=int(input("enter a width "))
+area=length*width
+perimeter=2*length*width
+print(area)
+print(perimeter)
+
+#Q4->Convert celsius into fahrenheit
+
+C=int(input("enter a celsius "))
+Fahrenheit=9/5+C+32
+print(Fahrenheit)
+
+#Q5->Find average of three numbers
+
+x=int(input("enter a first number "))
+y=int(input("enter a second number "))
+z=int(input("enter a third number "))
+average=x+y+z/3
+print(average)
