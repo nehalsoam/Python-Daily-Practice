7719424bc56e2e455aa86c40844310ddb243c672
Nehal Singh Soam <nehalnehal8434@gmail.com>
2026-08-26 00:43:55 +0530

    Uncommented code for various calculations

--- Day-02/Day-02.py
+++ Day-02/Day-02.py
@@ -1,41 +1,41 @@
 #Q1->check number is even or odd
-# num=int(input("enter a num: "))
-# result="Even"*(num%2==0)+"Odd"*(num%2==1)
-# print(result)45
+num=int(input("enter a num: "))
+result="Even"*(num%2==0)+"Odd"*(num%2==1)
+print(result)45
 
 #Q2->find total,average,percentage of 5subjects
-# s1=float(input("enter marks of s1: "))
-# s2=float(input("enter marks of s2: "))
-# s3=float(input("enter marks of s3: ")) 
-# s4=float(input("enter marks of s4: "))
-# s5=float(input("enter marks of s5: "))
-# total=s1+s2+s3+s4+s5
-# average=total/5
-# percentage=total/500*100
-# print("total marks= ",total)
-# print("average= ",average)
-# print("percentage= ",percentage,"%")
+s1=float(input("enter marks of s1: "))
+s2=float(input("enter marks of s2: "))
+s3=float(input("enter marks of s3: ")) 
+s4=float(input("enter marks of s4: "))
+s5=float(input("enter marks of s5: "))
+total=s1+s2+s3+s4+s5
+average=total/5
+percentage=total/500*100
+print("total marks= ",total)
+print("average= ",average)
+print("percentage= ",percentage,"%")
 
 #Q3->find Simple Interest
-# P=float(input("enter principle: "))
-# R=float(input("enter rate of interest: "))
-# T=float(input("enter time: "))
-# SI=P*R*T/100
-# print("Simple interest: ",SI)
+P=float(input("enter principle: "))
+R=float(input("enter rate of interest: "))
+T=float(input("enter time: "))
+SI=P*R*T/100
+print("Simple interest: ",SI)
 
 #Q4->find last digit 
-# num=int(input("enter a digit: "))
-# last_digit=num%10
-# print("last digit is: ",last_digit) 
+num=int(input("enter a digit: "))
+last_digit=num%10
+print("last digit is: ",last_digit) 
 
 #Q5->swap two number without using third 
-# a=5
-# b=10
-# a=a+b
-# b=a-b
-# a=a-b
-# print(a)
-# print(b)
+a=5
+b=10
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
 
 #Q6->convert total seconds into hours,minutes,seconds 
 total_sec=int(input("enter total sec: "))
